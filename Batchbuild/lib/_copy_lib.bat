8e1402b57f90f427ef177d43889bc8c6f0df1665
orax <0-F@users.noreply.github.com>
2025-07-11 16:17:12 +0200

    add _copy_lib.bat

---
+++ Batchbuild/lib/_copy_lib.bat
@@ -0,0 +1,13 @@
+copy /Y /V A:\Steam\steamapps\common\Valheim\BepInEx\core\0Harmony.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\BepInEx\core\BepInEx.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\assembly_valheim.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\UnityEngine.AssetBundleModule.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\UnityEngine.CoreModule.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\UnityEngine.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\UnityEngine.IMGUIModule.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\UnityEngine.PhysicsModule.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\UnityEngine.UI.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\gui_framework.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\Unity.TextMeshPro.dll .
+copy /Y /V A:\Steam\steamapps\common\Valheim\valheim_Data\Managed\assembly_utils.dll .
+@pause
